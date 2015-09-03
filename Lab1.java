0b6caad62de2257409a470729c5eb7036cb54acc
Michael Camara <camaram@aldenv134.allegheny.edu>
2015-09-03 14:58:23 -0400

    Part1 finished: added header, changed .class file through pbe to output specific values

---
+++ Lab1.java
@@ -0,0 +1,24 @@
+//*********************************************************************************
+// Honor Code: The work I am submitting is a result of my own thinking and efforts.
+// Michael Camara
+// CMPSC 220 Fall 2015 (Professor Roos)
+// Lab # 1
+// Date: 9/10/15
+// 
+// Purpose: Understanding the Java compiler and bytecode
+//
+//*******************************************************************************
+
+public class Lab1 {
+    public static void main(String[] args) {
+        int i = 5, j = 6, k = 127, l = 128, m = 255,
+            n = 32767, o = 32768;
+        System.out.println("i="+i);
+        System.out.println("j="+j);
+        System.out.println("k="+k);
+        System.out.println("l="+l);
+        System.out.println("m="+m);
+        System.out.println("n="+n);
+        System.out.println("o="+o);
+    }
+}
