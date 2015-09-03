f7e29ea43719e8ccec566ee9f5cd1d78d4083ab8
Michael Camara <camaram@aldenv134.allegheny.edu>
2015-09-03 15:55:14 -0400

    Working on lab1, on part 4

---
+++ lab1/Lab1Part4.java
@@ -0,0 +1,23 @@
+//*********************************************************************************
+// Honor Code: The work I am submitting is a result of my own thinking and efforts.
+// Michael Camara
+// CMPSC 220 Fall 2015 (Professor Roos)
+// Lab # 1
+// Date: 9/10/15
+// 
+// Purpose: Understanding the Java compiler and bytecode
+//
+//*******************************************************************************
+
+public class Lab1Part4 {
+    public static void main(String[] args) {
+        double i = -6000.53, j = -2.1, k = 0, l = 5, m = 128, n = 5000.7, o = 65000.8;
+        System.out.println("i="+i);
+        System.out.println("j="+j);
+        System.out.println("k="+k);
+        System.out.println("l="+l);
+        System.out.println("m="+m);
+        System.out.println("n="+n);
+        System.out.println("o="+o);
+    }
+}
