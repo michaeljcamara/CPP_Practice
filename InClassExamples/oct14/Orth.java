e877b626cd57c2ed40bf4d5f90a944308047e9c4
Michael Camara <michaeljcamara@gmail.com>
2017-03-09 16:43:41 -0500

    organizing repo, removing unrelated files

--- InClassExamples/oct14/Orth.java
+++
@@ -1,8 +0,0 @@
-public class Orth {
-  public static void main(String[] args) {
-    int a = 10, b = 20, c = 30, d = 30, e = 29, f = 30, g = 30;
-
-     if(a < b < c <= d > e != f == g)
-      System.out.println("yes");
-  }
-}
--- InClassExamples/oct14/orth.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main() {
-    int a = 10, b = 20, c = 30, d = 30, e = 29, f = 30, g = 30;
-
-     if(a < b < c <= d > e != f == g)
-       printf("yes");
-}
